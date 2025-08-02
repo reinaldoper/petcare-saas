5702e246171477d82130ad10e26562b0f87f4fac
reinaldoper <reinaldoper83@gmail.com>
2025-08-02 13:11:29 -0400

    fix: Ajusta dto appointment

--- src/appointment/dto/zod.dto.ts
+++ src/appointment/dto/zod.dto.ts
@@ -8,8 +8,8 @@ export const createAppointmentDtoSchema = z.object({
     .string()
     .min(10, MSG)
     .refine((val) => {
-      const date = new Date(val);
-      return !isNaN(date.getTime());
+      const date = new Date(val).toISOString();
+      return date;
     }, MSG),
   reason: z.string().min(5, REASON_MSG),
   petId: z.number().int().positive(PET_ID_MSG),
