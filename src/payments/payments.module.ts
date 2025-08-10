2ac0c2f429f526763710c4ad4c141d28703f0990
reinaldoper <reinaldoper83@gmail.com>
2025-08-10 01:03:06 -0400

    chore: Adiciona notificação com socketio

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { PaymentGateway } from './payment.gateway';
 
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService],
+  providers: [PaymentsService, PaymentGateway],
+  exports: [PaymentGateway],
 })
 export class PaymentsModule {}
